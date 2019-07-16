6f4399bf87815f7cb589686ec4e2a5c33c72953c
Alword <anzer987@yandex.ru>
2019-07-16 20:10:10 +0300

    Notes command template

--- EvernoteClone/ViewModel/NotesVM.cs
+++ EvernoteClone/ViewModel/NotesVM.cs
@@ -1,9 +1,31 @@
 ﻿using EvernoteClone.Model;
+using EvernoteClone.ViewModel.Commands;
+using System.Collections.ObjectModel;
 
 namespace EvernoteClone.ViewModel
 {
     public class NotesVM
     {
-        
+        public Notebook SelectedNotebook
+        {
+            get { return selectedNotebook; }
+            set
+            {
+                selectedNotebook = value; //TODO get notes
+            }
+        }
+        private Notebook selectedNotebook;
+        public ObservableCollection<Notebook> Notebooks { get; set; }
+        public ObservableCollection<Note> Notes { get; set; }
+
+        public NewNotebookCommand NewNotebookCommand { get; set; }
+
+        public NewNoteCommand NewNoteCommand { get; set; }
+
+        public NotesVM()
+        {
+            NewNotebookCommand = new NewNotebookCommand(this);
+            NewNoteCommand = new NewNoteCommand(this);
+        }
     }
 }
