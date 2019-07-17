d6879cefb5a1c8474eeb4adbedc9608113d87c22
Alword <anzer987@yandex.ru>
2019-07-17 12:57:20 +0300

    speech is unavaliable in russia

--- EvernoteClone/View/NotesWindow.xaml.cs
+++ EvernoteClone/View/NotesWindow.xaml.cs
@@ -11,6 +11,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Speech;
+using System.Speech.Recognition;
+using System.Threading;
+using System.Globalization;
 
 namespace EvernoteClone.View
 {
@@ -19,9 +23,42 @@ namespace EvernoteClone.View
     /// </summary>
     public partial class NotesWindow : Window
     {
+        private SpeechRecognitionEngine recognitionEngine;
+
         public NotesWindow()
         {
             InitializeComponent();
+        }
+
+        private void RecognitionEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            contentRichTextBox.Document.Blocks.Add(new Paragraph(new Run(e.Result.Text)));
+        }
+
+        private void Content_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int charactersCount = (new TextRange(contentRichTextBox.Document.ContentStart, contentRichTextBox.Document.ContentEnd)).Text.Length;
+            StatusTextBlock.Text = $"Document length: {charactersCount} characters";
+        }
+
+        private void BoldButton_Click(object sender, RoutedEventArgs e)
+        {
+            contentRichTextBox.Selection.ApplyPropertyValue(Inline.FontWeightProperty, FontWeights.Bold);
+        }
+
+        bool isRecognizing = false;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (isRecognizing)
+            {
+                recognitionEngine.RecognizeAsyncStop();
+            }
+            else
+            {
+                recognitionEngine.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            isRecognizing = !isRecognizing;
+            string textRange = new TextRange(contentRichTextBox.Document.ContentStart, contentRichTextBox.Document.ContentEnd).Text;
         }
     }
 }
